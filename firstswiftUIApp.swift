7473d83fefb242915a192e5317a73f9a0468d2a1
Derrkio <164192959+Derrkio@users.noreply.github.com>
2026-03-25 13:43:00 -0500

    Add files via upload

---
+++ firstswiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstswiftUIApp.swift
+//  firstswiftUI
+//
+//  Created by derrick muonaka on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct firstswiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
